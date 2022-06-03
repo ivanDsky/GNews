ad215e73732b2a03395d94d1286b5dd124ab9125
zloydi <dobrovolskyivan2002@gmail.com>
2022-06-03 10:17:11 +0300

    Added BindingFragment

---
+++ app/src/main/java/ua/zloydi/gnews/ui/core/BindingFragment.kt
@@ -0,0 +1,29 @@
+package ua.zloydi.gnews.ui.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BindingFragment<out VB : ViewBinding> : Fragment(){
+	private var _binding: VB? = null
+	val binding : VB = checkNotNull(_binding)
+	
+	abstract fun inflateBinding(inflater: LayoutInflater) : VB
+	
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = inflateBinding(inflater)
+		return binding.root
+	}
+	
+	override fun onDestroyView() {
+		_binding = null
+		super.onDestroyView()
+	}
+}
